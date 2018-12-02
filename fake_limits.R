b81fc8fad3c68ce457ae799969f5bf75b69afabb
ilja01 <45531960+ilja01@users.noreply.github.com>
2018-12-02 12:58:57 +0200

    added limits for 1st ret clients

--- fake_limits.R
+++ fake_limits.R
@@ -3,4 +3,9 @@ if (paid_loans_count==0) {
 	else if (score<500) limit=500
 	else if (score<700) limit=1000
 	else limit=2000
-}
+} else if (paid_loans_count==1) {
+	if (score<300) limit=300
+	else if (score<500) limit=700
+	else if (score<700) limit=1500
+	else limit=2000
+} 
