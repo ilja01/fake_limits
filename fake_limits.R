d10033d628d3e284ba0affb414551915d31fb2a2
SergejsDS <45793056+SergejsDS@users.noreply.github.com>
2018-12-11 17:38:16 +0200

    Increase/ decrease limits for new clients
    Double limit for one group, half for the other

--- fake_limits.R
+++ fake_limits.R
@@ -1,8 +1,8 @@
 #test comment from Krii
 if (paid_loans_count==0) {
 	if (score<300) limit=200
-	else if (score<500) limit=500
-	else if (score<700) limit=1000
+	else if (score<500) limit=500/2
+	else if (score<700) limit=1000*2
 	else limit=2000
 } else if (paid_loans_count==1) {
 	if (score<300) limit=300
